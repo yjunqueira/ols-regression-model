b7d8bdca2b92de740fc6e800a8cd2ae3c5a3255f
yjunqueira <52931504+yjunqueira@users.noreply.github.com>
2023-10-26 20:32:17 -0300

    Add files via upload

---
+++ run_granger_test_p-h.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import numpy as np
+from statsmodels.tsa.stattools import grangercausalitytests
+import matplotlib.pyplot as plt
+
+# Carregar os dados do arquivo XLSX
+dados = pd.read_excel('bitcoin15-17.xlsx')
+
+# Selecionar as variáveis a serem testadas
+data = dados[['marketprice', 'giga-hashrate']].dropna()
+
+# Executar o teste de Granger invertido
+max_lag = 3  # Número máximo de defasagens a serem consideradas
+results = grangercausalitytests(data, max_lag, verbose=False)
+
+# Exibir os resultados do teste de Granger como uma tabela
+for lag in results.keys():
+    print(f"Lag {lag}:")
+    print("------")
+    for key, value in results[lag][0].items():
+        print(f"{key}: {value}")
+    print("\n")
+
+# Plotar os resultados do teste de Granger invertido
+lags = range(1, max_lag+1)
+p_values = [results[lag][0]['ssr_chi2test'][1] for lag in lags]
+
+plt.bar(lags, p_values)
+plt.xlabel('Defasagem (lag)')
+plt.ylabel('Valor-p')
+plt.title('Teste de Granger: marketprice -> hashrate')
+plt.xticks(lags)
+plt.show()
